cbe6a2b75dc9134b52c190aed7a18fdaf93892c2
Assert1veUser <tony.markin2017@yandex.ru>
2024-10-26 12:50:25 +0300

    update

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7816985e71516574d561da7985e60ecbc3cfbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd508e06f5ebbce9782aea80a38450f6dbb4e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
